7cd82cad9ff8c45547d5c1cd1cc331c56df48c2e
Goutam Bilagikar <103488541+goutamb07@users.noreply.github.com>
2022-04-12 13:18:50 +0530

    Create dyck_paths.cpp

---
+++ dyck_paths.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fact(int n) {
+  int x = 1;
+  for (int i = 1; i <= n; x *= i, i++)
+    ;
+  return x;
+}
+
+int main() {
+  long long int n, catalan_no, temp;
+  cout << "Enter the value of n" << endl;
+  cin >> n;
+
+  temp = fact(n);
+  cout << temp << endl;
+  catalan_no = fact(2 * n) / (temp * temp);
+  catalan_no /= (n + 1);
+
+  cout << "Total no of dyck paths for " << n << " are " << catalan_no;
+}
